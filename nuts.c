4b3ae943f1047e9c6feabc54b7179e9e655f2246
Afaren <550047450@qq.com>
2016-03-18 14:30:05 +0800

    add a source file nuts.c

---
+++ nuts.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: nuts.c
+	> Author: Afar
+	> Mail: 550047450@qq.com 
+	> Created Time: Fri, Mar 18, 2016  2:28:37 PM
+ ************************************************************************/
+
+#include<stdio.h>
+int main(void){
+	printf("other nuts\n");
+	return 0;
+}
